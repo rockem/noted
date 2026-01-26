4ccd45650483fb6c465d8090aade76f34a56d796
eli segal <eli.segal@gmail.com>
2026-01-26 22:54:52 +0200

    eco system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use chrono::Local;
+use std::env;
+use std::fs::File;
+use std::path::{Path, PathBuf};
+
+fn get_store_path() -> PathBuf {
+    PathBuf::from(env::var("NOTED_STORE").unwrap())
+}
+
+fn get_daily_note_path(store_path: &Path) -> PathBuf {
+    let today = Local::now().format("%Y-%m-%d").to_string();
+    store_path.join(format!("{}.md", today))
+}
+
+fn create_daily_note(path: &PathBuf) -> std::io::Result<File> {
+    File::create(path)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let store_path = get_store_path();
+
+    let daily_note_path = get_daily_note_path(&store_path);
+
+    // Only create the daily note if it doesn't already exist
+    create_daily_note(&daily_note_path).unwrap();
 }
---
+++ tests/support/mod.rs
@@ -0,0 +1,5 @@
+mod app_driver;
+mod store_driver;
+
+pub use app_driver::AppDriver;
+pub use store_driver::StoreDriver;
